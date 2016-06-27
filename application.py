72050cae1e2881f151c4b7a5a9abb88d3094a7e6
glebofff <glebofff@gmail.com>
2016-06-27 16:55:35 +0500

    yaml-related:    sample_data.yml in cp1251
    #2

--- application.py
+++ application.py
@@ -1,40 +1,14 @@
 # -*- coding: utf-8 -*-
-
-CLASSMATES = [
-{  'name':'Громова Дарья',
-    'url':'https://www.facebook.com/gromova.da'
-},
-
-{ 
-  'name':'Слободник Алексей',
-   'url':'https://www.facebook.com/markus.duck.90'
-   },
-
-{ 
-  'name':'Головизнин Алексей',
-   'url':'https://www.facebook.com/AirVetra'
-  },
-
-{ 
-  'name':'Каминский Денис',
-   'url':'https://www.facebook.com/dennis.kaminsky.3',
-   'aff':'fut.ru'
-},
-
-{
-  'name':'Кузнецова (Ромашова) Екатерина',
-   'url':'https://www.facebook.com/kate.romashova',
- }
-] 
-
 from flask import Flask, render_template
-application = Flask("application")
-
 from operator import itemgetter
+from utils import yaml
+
+application = Flask("application")
 
 @application.route("/")
 def hello():
-    return render_template("table.html", persons=sorted(CLASSMATES, key=itemgetter('name')))
+    classmates = yaml.loadfile('sample_data.yml')
+    return render_template("table.html", persons=sorted(classmates, key=itemgetter('name')))
      
 
 if __name__ == "__main__":
