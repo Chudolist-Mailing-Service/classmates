78c1a41a2d7d8500d986c8fa3d5cba729344a513
glebofff <glebofff@gmail.com>
2016-06-27 17:33:33 +0500

    removed 'utils' subfolder

--- application.py
+++ application.py
@@ -1,16 +1,15 @@
 # -*- coding: utf-8 -*-
 from flask import Flask, render_template
 from operator import itemgetter
-from utils import yaml
+from yaml_import import CLASSMATES
 
 application = Flask("application")
 
+
 @application.route("/")
 def hello():
-    classmates = yaml.loadfile('sample_data.yml')
-    return render_template("table.html", persons=sorted(classmates, key=itemgetter('name')))
+    return render_template("table.html", persons=sorted(CLASSMATES, key=itemgetter('name')))
      
 
 if __name__ == "__main__":
-
     application.run()
--- yaml_import.py
+++ yaml_import.py
@@ -1,37 +1,52 @@
-CLASSMATES = [
-{  'name':'Громова Дарья',
-    'url':'https://www.facebook.com/gromova.da'
-},
-
-{ 
-  'name':'Слободник Алексей',
-   'url':'https://www.facebook.com/markus.duck.90'
-   },
-
-{ 
-  'name':'Головизнин Алексей',
-   'url':'https://www.facebook.com/AirVetra'
-  },
-
-{ 
-  'name':'Каминский Денис',
-   'url':'https://www.facebook.com/dennis.kaminsky.3',
-   'aff':'fut.ru'
-},
-
-{
-  'name':'Кузнецова (Ромашова) Екатерина',
-   'url':'https://www.facebook.com/kate.romashova',
- }
-] 
-
 import yaml
+import chardet
+import codecs
+
+# CLASSMATES = [
+#     {  'name':'Громова Дарья',
+#        'url':'https://www.facebook.com/gromova.da'
+#        },
+#
+#     {
+#         'name':'Слободник Алексей',
+#         'url':'https://www.facebook.com/markus.duck.90'
+#     },
+#
+#     {
+#         'name':'Головизнин Алексей',
+#         'url':'https://www.facebook.com/AirVetra'
+#     },
+#
+#     {
+#         'name':'Каминский Денис',
+#         'url':'https://www.facebook.com/dennis.kaminsky.3',
+#         'aff':'fut.ru'
+#     },
+#
+#     {
+#         'name':'Кузнецова (Ромашова) Екатерина',
+#         'url':'https://www.facebook.com/kate.romashova',
+#     }
+# ]
+
+
+def loadfile(name, encoding=None, **kwargs):
+    with open(name, 'rb') as content_file:
+        content = content_file.read()  # type: bytes
+        cd = chardet.detect(content)
+    enc = encoding or cd.get('encoding', 'ascii')
+
+    return yaml.load(content.decode(enc), **kwargs)
+
+
+def dumps(data, encoding=None, **kwargs):
+    res = yaml.dump(data, **kwargs)
+    if encoding:
+        return codecs.unicode_escape_decode(res)[0].encode(encoding)
+    return res
 
 FB_PREFIX = "https://www.facebook.com/"
-
-with open('classmates.txt', 'r') as stream:
-   print(yaml.load(stream))
- 
+CLASSMATES = loadfile('classmates.txt', encoding='cp1251')
 # currently get an error:  
 # yaml.scanner.ScannerError: mapping values are not allowed here
 # in "classmates.txt", line 2, column 5
