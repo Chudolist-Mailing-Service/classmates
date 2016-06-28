eed8efb8eb230f6dd1beb6cc059a85c7bc11e0f0
=epogrebnyak <e.pogrebnyak@gmail.com>
2016-06-28 09:52:56 +0300

    moved blocks of code around files, but assert failing

--- application.py
+++ application.py
@@ -1,15 +1,37 @@
 # -*- coding: utf-8 -*-
+import yaml
+import chardet
+import codecs
+
 from flask import Flask, render_template
 from operator import itemgetter
-from yaml_import import classmates
 
 application = Flask("application")
+main_page_tamplate_file = "table.html"
 
 
+def loadfile(name, encoding=None, **kwargs):
+    with open(name, 'rb') as content_file:
+        content = content_file.read()  # type: bytes
+        cd = chardet.detect(content)
+    enc = encoding or cd.get('encoding', 'ascii')
+
+    return yaml.load(content.decode(enc), **kwargs)
+
+def dumps(data, encoding=None, **kwargs):
+    res = yaml.dump(data, **kwargs)
+    if encoding:
+        return codecs.unicode_escape_decode(res)[0].encode(encoding)
+    return res
+
+def get_classmates(file='classmates.txt', pivot='name'):
+    classmates = loadfile(file)
+    return sorted(classmates, key=itemgetter(pivot))
+    
 @application.route("/")
 def hello():
-    return render_template("table.html", persons=sorted(classmates, key=itemgetter('name')))
-     
+    classmates = get_classmates()
+    return render_template(main_page_tamplate_file, persons=classmates)
 
 if __name__ == "__main__":
     application.run()
--- yaml_import.py
+++ yaml_import.py
@@ -28,23 +28,9 @@ CLASSMATES = [
     }
 ]
 
-def loadfile(name, encoding=None, **kwargs):
-    with open(name, 'rb') as content_file:
-        content = content_file.read()  # type: bytes
-        cd = chardet.detect(content)
-    enc = encoding or cd.get('encoding', 'ascii')
+from application import get_classmates 
 
-    return yaml.load(content.decode(enc), **kwargs)
-
-
-def dumps(data, encoding=None, **kwargs):
-    res = yaml.dump(data, **kwargs)
-    if encoding:
-        return codecs.unicode_escape_decode(res)[0].encode(encoding)
-    return res
-
-FB_PREFIX = "https://www.facebook.com/"
-classmates = loadfile('classmates.txt')
+classmates = get_classmates()
 
 for i in range(0, len(CLASSMATES)):
     C = CLASSMATES[i]
@@ -55,7 +41,3 @@ for i in range(0, len(CLASSMATES)):
         raise Exception('%s is not equal to constant %s' % (c, C))
     except IndexError:
         raise
-
-# currently get an error:  
-# yaml.scanner.ScannerError: mapping values are not allowed here
-# in "classmates.txt", line 2, column 5
