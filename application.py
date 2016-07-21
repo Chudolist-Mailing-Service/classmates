17cbb89d68b3bfb184469bcc855158caa2d312f0
=epogrebnyak <e.pogrebnyak@gmail.com>
2016-07-22 01:58:24 +0300

    workable, no 00

--- application.py
+++ application.py
@@ -1,20 +1,25 @@
 # -*- coding: utf-8 -*-
+import os 
+import json
 from flask import Flask, render_template
-from operator import itemgetter
+#from operator import itemgetter
 
-from userdata.from_xl import get_printable  
 
 application = Flask("application")
 MAIN_PAGE_TEMPLATE_FILE = "table.html"
+CLASSMATES_JSON_PATH = os.path.join('userdata', 'classmates.json')
+
 
 def get_classmates():
-    return [{'name':x[0], 'url':"https://www.facebook.com/" + x[1]} for x in get_printable()]
-    
+    with open(CLASSMATES_JSON_PATH, 'r') as file:
+        return json.load(file)
+        
 @application.route("/")
 def hello():
-    classmates = get_classmates()
-    return render_template(MAIN_PAGE_TEMPLATE_FILE, persons=sorted(classmates, key=itemgetter('name')))
+    classmates = get_classmates()#.sort(key=itemgetter('name'))
+    return render_template(MAIN_PAGE_TEMPLATE_FILE, persons=classmates)
+
 
 if __name__ == "__main__":
-    print(get_classmates())
+    #print(get_classmates())
     application.run(debug = True)
--- userdata/from_xl.py
+++ userdata/from_xl.py
@@ -1,11 +1,19 @@
 import xlrd
 import os
+import csv
+import json
 
 XL_PATH = os.path.join('classmates.xls')
+FMT = {'delimiter':';', 'lineterminator':"\n"}
 
-def read_from_csv(filename):
-    with open(filename, 'r') as csvfile:    
-        return list(csv.reader(csvfile, **FMT)) 
+def read_from_csv(filename, fieldnames):
+    with open(filename, 'r') as csvfile:
+        for x in csv.DictReader(csvfile, fieldnames=fieldnames,**FMT):
+            yield x
+
+def get_ids():
+    iterable = read_from_csv(filename='ids.csv',fieldnames=['fb_name','id'])
+    return {x['fb_name']:x['id'] for x in iterable}
 
 def get_full_classlist():
     rb = xlrd.open_workbook(XL_PATH)
@@ -15,14 +23,22 @@ def get_full_classlist():
         yield(row)
 
 def get_printable():
+    d = get_ids()
     for x in get_full_classlist():
-        if x[0] and x[5]:
-            name = ' '.join([x[0],x[1]])
-            id = str(int(x[5]))
-            yield name, id
+        if x[0] and x[4]:
+            yield {'name': ' '.join([x[0],x[1]]).strip(),
+                    'url':"https://www.facebook.com/" + d[x[4]] 
+                   }
             
-if __name__ == "__main__": 
-  k = read_from_csv('ids.csv')
-  z = list(get_full_classlist())
-  a = [x for x in z if x[4]]
+if __name__ == "__main__":
+
+  a = list(get_printable())
   
+  with open('classmates.json', 'w') as file:
+      json.dump(a,file)
+      
+  with open('classmates.json', 'r') as file:
+      b = json.load(file)
+      
+      
+  assert a == b
