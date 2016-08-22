92ddbc1c4b5b2bb570d1a9c857516396310dc1e1
Vidadi Shidaev <viddik13@gmail.com>
2016-08-23 02:03:51 +0300

    Changes: - Spaces deleted by IDE - Added facebook button in 'user.html'

--- application.py
+++ application.py
@@ -1,11 +1,14 @@
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """Read classmates data from json, display as full list, by group or as individual page.
    Also allows to collect missing information about classmates FB ids. 
 """
 
 import json
-from flask import Flask, render_template
+from flask import Flask, render_template, session, request, url_for, redirect
 from operator import itemgetter
+from flask_oauthlib.client import OAuth, OAuthException, OAuthResponse
+from config import FACEBOOK_APP_ID, FACEBOOK_APP_SECRET, FACEBOOK_CALLBACK
 
 # will require update to req.txt
 from transliterate import translit
@@ -17,6 +20,22 @@ GROUP_PAGE_TEMPLATE = "group.html"
 USER_PAGE_TEMPLATE  = "user.html"
 
 CLASSMATES_JSON_PATH = 'classmates.json'
+
+# FB auth setup
+oauth = OAuth()
+
+facebook = oauth.remote_app(
+    'facebook',
+    consumer_key=FACEBOOK_APP_ID,
+    consumer_secret=FACEBOOK_APP_SECRET,
+    request_token_params={'scope': 'public_profile'},
+    base_url='https://graph.facebook.com',
+    request_token_url=None,
+    access_token_url='/oauth/access_token',
+    access_token_method='GET',
+    authorize_url='https://www.facebook.com/dialog/oauth'
+)
+
 
 def get_classmates():
     """Return list of dictionaries holding classmates data."""
@@ -51,14 +70,15 @@ def pick_dict_by_translit(translit_name):
     return [p for p in PEOPLE if p['translit'] == translit_name][0]  
         
 @application.route("/")
-def hello():      
-    return render_template(MAIN_PAGE_TEMPLATE, persons=PEOPLE)
+def hello():
+    return render_template(MAIN_PAGE_TEMPLATE, persons=PEOPLE       )
 
     
 @application.route('/user/<username>')
 def show_user_profile(username):         
     if username in TRANSLIT_NAMES:
         p = pick_dict_by_translit(username)
+        session['username'] = username
         return render_template(USER_PAGE_TEMPLATE, person=p)        
     else:
         return 'Cannot find %s' % username
@@ -70,7 +90,48 @@ def show_post(group_n):
     else:
         return 'Illegal group number: %d' % group_n
     
-    
+
+# ---- FACEBOOK ----
+@application.route('/oauth/authorize/facebook', methods=['GET', 'POST'])
+def oauth_authorize_facebook():
+    return facebook.authorize(callback=FACEBOOK_CALLBACK)
+
+
+@facebook.tokengetter
+def get_facebook_oauth_token():
+    return session.get('facebook_token')
+
+
+@application.route('/oauth/callback/facebook/', methods=['GET', 'POST'])
+def oauth_authorized_fb():
+    next_url = request.args.get('next') or url_for('hello')
+    resp = facebook.authorized_response()
+
+    if resp is None:
+        return 'Access denied: reason=%s error=%s' % (
+            request.args['error_reason'],
+            request.args['error_description']
+        )
+
+    if isinstance(resp, OAuthException):
+        return 'OAuthException: %s' % resp.message
+
+    session['facebook_token'] = (resp['access_token'], '')
+    me = facebook.get('/me?fields=id')
+    u_id = me.data['id']
+
+    url = "https://www.facebook.com/" + u_id
+    p = pick_dict_by_translit(session['username'])
+    p['url'] = url
+
+    new_data = [user if user['translit']!=session['username'] else p for user in PEOPLE]
+
+    with open('classmates.json', 'w+') as outfile:
+        json.dump(new_data, outfile)
+
+    return redirect(next_url)
+
+
 if __name__ == "__main__":
     print("""TODO:
     Major:
@@ -94,5 +155,9 @@ if __name__ == "__main__":
     - storing personal information securely
     
     """)
+
+    application.secret_key = 'super secret key'
     application.run(debug = True)
-    
+
+
+
---
+++ config.py
@@ -0,0 +1,4 @@
+# -- Facebook auth
+FACEBOOK_APP_ID = '1092355147486878'
+FACEBOOK_APP_SECRET = '396d1b23784ede7ce2eab83fff30bd5b'
+FACEBOOK_CALLBACK = 'http://localhost:5000/oauth/callback/facebook'
